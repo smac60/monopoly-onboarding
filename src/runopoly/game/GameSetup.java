3f9a0d4b398a98894ec0defacd2dc77e2696861c
stephenkruse <stephenkruse36@gmail.com>
2015-09-02 15:33:47 -0500

    updated a few files

--- src/runopoly/game/GameSetup.java
+++ src/runopoly/game/GameSetup.java
@@ -2,5 +2,19 @@ package runopoly.game;
 
 public class GameSetup {
 
+	public GameSetup()
+	{
+		this.getClass();
+	}
 	
+	public boolean initializeGame(int numOfPlayers)
+	{
+		boolean didItWork = false;
+		for (int i =0;i==numOfPlayers;i++)
+		{ 
+			
+			
+		}
+		return didItWork;
+	}
 }
---
+++ test/runopoly/game/TestGameSetupAspects.java
@@ -0,0 +1,24 @@
+package runopoly.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGameSetupAspects {
+
+	@Test
+	public void testGameSetupCreatesCorrectNumberOfPlayers() {
+		
+		fail("Not yet implemented");
+	}
+
+//	@Test
+//	public void testDiceResultIsBetweeMinAndMax()
+//	{
+//		dice.setMaxValue(6);
+//		result = dice.rollDice();
+//		boolean isWithinRange = isResultWithinMinAndMax(result);
+//		assertTrue(isWithinRange);
+//		//fail("Not yet implemented");
+//	}
+}
